9216ed2ff35c22ab6befb7c1d6fadbc53833db31
Roman Chernobrovkin <Roman.Chernobrovkin@myib.com>
2020-08-28 18:44:10 +0300

    Add skeleton of solution.

---
+++ Code/JsGrid.Blazor.ComponentsLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JsGrid.Blazor.ComponentsLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ Code/JsGrid.Blazor.Contracts/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JsGrid.Blazor.Contracts
+{
+    public class Class1
+    {
+    }
+}
