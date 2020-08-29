592d76c4bf959760dce12cd633caca84569b4390
Roman Chernobrovkin <Roman.Chernobrovkin@myib.com>
2020-08-29 16:05:47 +0300

    Initial setup for very basic functionality.

---
+++ Code/JsGrid.Blazor.ComponentsLibrary/JsGridSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace JsGrid.Blazor.ComponentsLibrary
+{
+    public class JsGridSettings
+    {
+        public bool Inserting { get; set; }
+        public bool Editing { get; set; }
+        public bool Sorting { get; set; }
+        public bool Paging { get; set; }
+    }
+}
